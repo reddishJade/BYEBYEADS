2d0d3566a68f0a62d706f36d8779cac9681a2627
dwei <dwei2615@gmail.com>
2025-01-17 23:27:52 +0800

    chore: remove tmp directory from git and add to .gitignore

---
+++ data/python/dl.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+import time
+import shutil
+import requests
+
+# 删除目录下所有的文件
+directory = "./data/rules/"
+
+# 确保目录存在并遍历删除其中的文件
+if os.path.exists(directory):
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f"无法删除文件: {file_path}, 错误: {e}")
+else:
+    print(f"目录 {directory} 不存在")
+
+# 删除目录本身
+try:
+    shutil.rmtree(directory)
+    print(f"成功删除目录 {directory} 及其中的所有文件")
+except Exception as e:
+    print(f"无法删除目录 {directory}, 错误: {e}")
+
+# 创建临时文件夹
+os.makedirs("./tmp/", exist_ok=True)
+
+# 复制补充规则到tmp文件夹
+# shutil.copy("./data/mod/adblock.txt", "./tmp/adblock01.txt")
+# shutil.copy("./data/mod/whitelist.txt", "./tmp/allow01.txt")
+
+# 拦截规则
+adblock = [
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_10_Useful/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt",
+    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
+    "https://easylist-downloads.adblockplus.org/easylist.txt",
+    "https://easylist-downloads.adblockplus.org/easylistchina.txt",
+    "https://easylist-downloads.adblockplus.org/easyprivacy.txt",
+    "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
+    "https://raw.githubusercontent.com/d3ward/toolz/master/src/d3host.txt",
+    "https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/adblock.txt",
+    "https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt",
+    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
+    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
+    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt"
+]
+
+# 白名单规则
+allow = [
+    "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/allowlist.txt"
+]
+
+# 下载函数
+def download_file(url, output_path):
+    try:
+        response = requests.get(url, timeout=60)
+        response.raise_for_status()  # 检查请求是否成功
+        with open(output_path, 'wb') as file:
+            file.write(response.content)
+        print(f"成功下载: {url} 到 {output_path}")
+    except Exception as e:
+        print(f"下载失败: {url}, 错误: {e}")
+
+# 下载拦截规则
+for i, adblock_url in enumerate(adblock):
+    output_path = os.path.join("./tmp/", f"adblock{i}.txt")
+    download_file(adblock_url, output_path)
+    time.sleep(1)
+
+# 下载白名单规则
+for j, allow_url in enumerate(allow):
+    output_path = os.path.join("./tmp/", f"allow{j}.txt")
+    download_file(allow_url, output_path)
+    time.sleep(1)
+
+print('规则下载完成')
---
+++ data/python/merge.py
@@ -0,0 +1,88 @@
+import os
+import subprocess
+import glob
+import re
+import shutil
+from pathlib import Path
+
+os.chdir('tmp')
+
+print("合并上游拦截规则")
+file_list = glob.glob('adblock*.txt')
+with open('combined_adblock.txt', 'w', encoding='utf-8') as outfile:
+    for file in file_list:
+        with open(file, 'r', encoding='utf-8') as infile:
+            outfile.write(infile.read())
+        outfile.write('\n')
+
+with open('combined_adblock.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
+    content = re.sub(r'^[!].*$\n', '', content, flags=re.MULTILINE)
+    content = re.sub(r'^#(?!\s*#).*\n?', '', content, flags=re.MULTILINE)
+
+with open('cleaned_adblock.txt', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("拦截规则合并完成")
+
+print("合并上游白名单规则")
+allow_file_list = glob.glob('allow*.txt')
+with open('combined_allow.txt', 'w', encoding='utf-8') as outfile:
+    for file in allow_file_list:
+        with open(file, 'r', encoding='utf-8') as infile:
+            outfile.write(infile.read())
+        outfile.write('\n')
+
+with open('combined_allow.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
+    content = re.sub(r'^[!].*$\n', '', content, flags=re.MULTILINE)
+    content = re.sub(r'^#(?!\s*#).*\n?', '', content, flags=re.MULTILINE)
+
+with open('cleaned_allow.txt', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("白名单规则合并完成")
+
+print("过滤白名单规则")
+with open('cleaned_allow.txt', 'r', encoding='utf-8') as f:
+    allow_lines = f.readlines()
+
+with open('combined_adblock.txt', 'a', encoding='utf-8') as outfile:
+    outfile.writelines(allow_lines)
+
+with open('combined_adblock.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+with open('allow.txt', 'w', encoding='utf-8') as f:
+    for line in lines:
+        if line.startswith('@'):
+            f.write(line)
+
+current_dir = os.getcwd()
+adblock_file = os.path.join(current_dir, 'cleaned_adblock.txt')
+allow_file = os.path.join(current_dir, 'allow.txt')
+target_dir = os.path.join(current_dir, '.././data/rules/')
+Path(target_dir).mkdir(parents=True, exist_ok=True)
+adblock_file_new = os.path.join(target_dir, 'adblock.txt')
+allow_file_new = os.path.join(target_dir, 'allow.txt')
+os.rename(adblock_file, adblock_file_new)
+os.rename(allow_file, allow_file_new)
+
+print("规则去重中")
+os.chdir(".././data/rules/")  # 更改当前目录
+files = os.listdir()  # 得到文件夹下的所有文件名称
+result = []
+for file in files:  # 遍历文件夹
+    if not os.path.isdir(file):  # 判断是否是文件夹，不是文件夹才打开
+        if os.path.splitext(file)[1] == '.txt':
+            # print('开始去重'+(file))
+            with open(file, encoding="utf-8") as f:  # 打开文件
+                result = list(set(f.readlines()))
+                result.sort()
+            with open('test' + (file), "w", encoding="utf-8") as fo:
+                fo.writelines(result)
+            os.remove(file)
+            os.rename('test' + (file), (file))
+            # print((file) + '去重完成')
+
+print("规则去重完成")
